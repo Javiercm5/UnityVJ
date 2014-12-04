3f37da5201a2cc7665976803c61e17aad81249f3
Javier <javiercm5@gmail.com>
2014-12-04 13:30:11 +0100

    Main menu added

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject chapterMenuPanel;
+	public GameObject menuPanel;
+
+	void Start () {
+		chapterMenuPanel.SetActive(false);
+	}
+	
+	void Update () {
+	
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void ExitGame()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+
+	}
+
+	public void ShowChapterMenu()
+	{
+		chapterMenuPanel.SetActive(true);
+		menuPanel.SetActive(false);
+
+		/*foreach(Button b in menuPanel.GetComponentsInChildren<Button>()){
+			b.interactable = false;
+		}*/
+		//chapterMenu.GetComponent<Animator>().SetBool("chapterMenuSelected", false);
+
+	}
+
+	public void ShowMainMenu()
+	{
+		chapterMenuPanel.SetActive(false);
+		menuPanel.SetActive(true);
+		//chapterMenuPanel.GetComponent<Animator>().SetBool("mainMenuSelected", true);
+
+		/*foreach(Button b in menuPanel.GetComponentsInChildren<Button>()){
+			b.interactable = true;
+		}*/
+	}
+
+
+	public void SelectChapter(int lvlNumber)
+	{
+		Application.LoadLevel(lvlNumber);
+
+	}
+
+	public void ShowCredits()
+	{
+
+	}
+}
