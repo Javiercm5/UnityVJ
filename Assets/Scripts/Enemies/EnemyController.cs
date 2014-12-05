21102ad3468f9d6b3b193898adaca5b70f9db554
GerardPF <usuari.19@gmail.com>
2014-12-05 12:03:24 +0100

    navmesh

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,52 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
-
+public class EnemyController : MonoBehaviour
+{
 	public int attackDamage = 10;
 	public float attackDelay = 1.0f;
+	public float distFollowPlayer = 3.0f;
 
 	private GameObject player;
 	private PlayerHealth playerHealth;
 	private bool playerInRange = false;
 	private float timer;
+	private NavMeshAgent agent;
 
-
-	void Awake () {
+	void Awake()
+	{
 		player = GameObject.FindGameObjectWithTag("Player");
-		playerHealth = player.GetComponentInChildren <PlayerHealth> ();
+		playerHealth = player.GetComponentInChildren<PlayerHealth>();
 
+		agent = GetComponent<NavMeshAgent>();
 	}
-	
 
-	void Update () {
+	void Update()
+	{
 		timer += Time.deltaTime;
-		if(playerInRange && timer >= attackDelay) Attack ();
-	}
+		if(playerInRange && timer >= attackDelay) Attack();
 
+		if(agent)
+		{
+			Vector3 difference = player.transform.position - transform.position;
+			float distance = Mathf.Sqrt(Mathf.Pow(difference.x, 2.0f) + Mathf.Pow(difference.y, 2.0f) + Mathf.Pow(difference.z, 2.0f));
+
+			if(distance <= distFollowPlayer) agent.SetDestination(player.transform.position);
+			else agent.Stop();
+		}
+	}
 
 	void Attack()
 	{
 		timer = 0.0f;
-		playerHealth.TakeDamage(attackDamage);		
-		
-
+		playerHealth.TakeDamage(attackDamage);
 	}
 
-
-	void OnTriggerEnter (Collider hit)
+	void OnTriggerEnter(Collider hit)
 	{
-		if(hit.gameObject == player){
-			playerInRange = true;
-		}
+		if(hit.gameObject == player) playerInRange = true;
 	}
-
 
 	void OnTriggerExit(Collider hit)
 	{
-		if(hit.gameObject == player){
-			playerInRange = false;
-		}
+		if(hit.gameObject == player) playerInRange = false;
 	}
-
 }
