2aeef12b0dd2298514a1a0c7c9f453df36f86e3e
Javier <javiercm5@gmail.com>
2014-12-12 12:00:55 +0100

    finished spider area and playerDie on falling

---
+++ Assets/Scripts/dieFallScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dieFallScript : MonoBehaviour {
+
+	GameObject player;
+	PlayerHealth playerHealth;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealth = player.GetComponentInChildren<PlayerHealth>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if(hit.gameObject == player){
+			playerHealth.Die();		
+		}
+	}
+}
